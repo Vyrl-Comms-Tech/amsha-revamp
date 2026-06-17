83f32911e22b337e0ec832da5dc2b7430b6a034d
Vyrl-Comms-Tech <vyrlcommstech@gmail.com>
2026-06-17 16:31:51 +0500

    Fix the Service inner

--- app/services-inner/page.jsx
+++ app/services-inner/page.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import ServicesInnerHero from '@/app/components/ServicesInner/ServiceInnerHero'
-import SupportAreas from '@/app/components/ServicesInner/SupportAreas'
-import OtherInner from '@/app/components/ServicesInner/otherInner'
+import ServiceInnerHero from '../components/ServicesInner/ServiceInnerHero'
+import SupportAreas from '../components/ServicesInner/SupportAreas'
+import OtherInner from '../components/ServicesInner/otherInner'
 
 const page = () => {
   return (
     <div>
-      <ServicesInnerHero />
+      <ServiceInnerHero />
       <SupportAreas />
       <OtherInner />
-
     </div>
   )
 }
